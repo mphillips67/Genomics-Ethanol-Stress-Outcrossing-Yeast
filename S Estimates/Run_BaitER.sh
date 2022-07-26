f862faa4e7c2ffb1014ad73439e35c6ff5abe880
mphillips67 <mphillips6789@gmail.com>
2022-07-26 10:15:29 -0700

    Add files via upload

---
+++ S Estimates/Run_BaitER.sh
@@ -0,0 +1,6 @@
+#follow instructions to install BaitER and dependencies found here:https://github.com/mrborges23/Bait-ER.
+#Run the following to generate results for each group of populations (create sync files first!)
+
+./baiter High.cf
+./baiter Moderate.cf
+./baiter Controls.cf
