11d45b720f16086d73a531fe342b492b735e8992
mphillips67 <mphillips6789@gmail.com>
2023-06-28 11:57:48 -0700

    Add files via upload

---
+++ S Estimates/Combine_results.R
@@ -0,0 +1,15 @@
+setwd("~/Dropbox/Ethanol expression project/Genomic_analysis/Ne_S/Bait-ER")
+
+#read in results
+control <- read.table("Control_BaitER_results.txt", header = TRUE)
+mod <- read.table("Moderate_BaitER_results.txt", header = TRUE)
+high <- read.table("High_BairER_results.txt", header = TRUE)
+
+#combine
+control_s <- control$sigma
+mod_s <- mod$sigma
+high_s <- high$sigma
+
+data <- cbind(control[,1:3],control_s,mod_s,high_s)
+
+write.table (data,file="BaitER_results_all.txt",quote=FALSE,row.names = FALSE, col.names = TRUE, sep = "\t")
